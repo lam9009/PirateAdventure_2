730dbe59584cfa1a33302920bdf87f1620f6540d
lam9009 <lam9009@yahoo.com>
2015-02-11 17:48:23 -0800

    Initial Commit

---
+++ PirateAdventure_2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PirateAdventure_2
+//
+//  Created by Alfred on 11/2/15.
+//  Copyright (c) 2015 Maptier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PirateAdventure_2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PirateAdventure_2
+//
+//  Created by Alfred on 11/2/15.
+//  Copyright (c) 2015 Maptier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
